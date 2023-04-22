9c009051222e619488b08321fe3c26a175b52800
Suslicke <suslicketeam@gmail.com>
2023-04-23 02:39:21 +0400

    upd v2

---
+++ ReadME.md
@@ -0,0 +1,1 @@
+# Telegram bot V 1.0
--- tg_bot/bot.py
+++ tg_bot/bot.py
@@ -4,14 +4,15 @@ sys.path.append('')
 
 from config import TELEGRAM_TOKEN
 from aiogram import Bot, Dispatcher, executor
-import commands
 
 bot = Bot(token=TELEGRAM_TOKEN)
 
+
 dp = Dispatcher(bot)
 
-commands.setup(dp)
 
 if __name__ == '__main__':
+    from tg_bot.commands import setup
+    setup(dp)
     executor.start_polling(dp, skip_updates=True)
 
